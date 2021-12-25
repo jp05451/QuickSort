bcb6ecb9bf743199a9ce255221d38e45f33f597d
jp05451 <jp05451@gmail.com>
2021-12-26 04:13:21 +0800

    first commit

---
+++ quickSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+template<class C>
+int partition(C arr[], int left, int right, int loc)
+{
+    bool flag = 0;
+    while (flag == 0)
+    {
+        while (arr[loc] <= arr[right] && loc != right)
+        {
+            right--;
+        }
+        if (loc == right)
+        {
+            flag = 1;
+        }
+        else if (arr[loc] > arr[right])
+        {
+            swap(arr[loc], arr[right]);
+            loc = right;
+        }
+
+        if (flag == 0)
+        {
+            while (arr[loc] >= arr[left] && loc != left)
+            {
+                left++;
+            }
+            if (loc == left)
+            {
+                flag = 1;
+            }
+            else if (arr[loc] < arr[left])
+            {
+                swap(arr[loc], arr[left]);
+                loc = left;
+            }
+        }
+    }
+    return loc;
+}
+
+template<class T>
+void quickSort(T arr[], int begin, int end)
+{
+    if (begin < end)
+    {
+        loc = partition(arr, begin, end, begin);
+        quickSort(arr, begin, loc - 1);
+        quickSort(arr, loc + 1, end);
+    }
+}
